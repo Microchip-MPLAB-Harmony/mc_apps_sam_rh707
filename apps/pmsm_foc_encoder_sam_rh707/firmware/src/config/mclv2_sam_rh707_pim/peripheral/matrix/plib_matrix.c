478df16a2a5c5f99acde877bf2d79ecdf5fe30e6
Binod Sharma <Binod.Sharma@microchip.com>
2024-10-29 11:19:06 +0530

    [DFT] DFT regeneration and test of encoder based sensored FOC example project on dsPICDEM MCLV2 MH3-98038

---
+++ apps/pmsm_foc_encoder_sam_rh707/firmware/src/config/mclv2_sam_rh707_pim/peripheral/matrix/plib_matrix.c
@@ -0,0 +1,78 @@
+/*******************************************************************************
+  Matrix (AHB) PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_matrix.c
+
+  Summary:
+    AHB Matrix PLIB implementation file
+
+  Description:
+    Configure AHB masters and slaves.
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include <device.h>
+#include "plib_matrix.h"
+
+// *****************************************************************************
+
+void MATRIX_Initialize(void)
+{
+    /* Set FlexRAM memory in user mode */
+    MATRIX0_REGS->MATRIX_PASSR[0]= 0xFF;
+    MATRIX0_REGS->MATRIX_PSR[0]= 0x00030303;
+    MATRIX0_REGS->MATRIX_PASSR[1]= 0xFF;
+    MATRIX0_REGS->MATRIX_PSR[1]= 0x00030303;
+    MATRIX0_REGS->MATRIX_PASSR[7]= 0xFF;
+    MATRIX0_REGS->MATRIX_PSR[7]= 0x00030303;
+
+    /* Set AHB Slave in user mode */
+    MATRIX0_REGS->MATRIX_PSR[6] = 0x00070707;
+    MATRIX0_REGS->MATRIX_PASSR[6] = 0x00000444;
+
+    /* Set HEFC TOP value and set memory in user mode */
+    MATRIX0_REGS->MATRIX_PASSR[2] = 0x5;
+    MATRIX0_REGS->MATRIX_PSR[2] = 0x00010101;
+    MATRIX0_REGS->MATRIX_PRTSR[2] = 0x5;
+
+    /* Enable Master Remap Control for SPW, IP1553, ICM and CRCCU to access address 0 */
+    MATRIX0_REGS->MATRIX_MRCR |= (1UL << 9U) | (1UL << 10U) | (1UL << 11U) | (1UL << 12U);
+}
+
+/*******************************************************************************
+ End of File
+*/
